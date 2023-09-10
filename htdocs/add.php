54ee0044525e354a9dbbd0ff86fd617cf91e7340
Buddhika Nipun <bnsbuddhika@gmail.com>
2023-09-11 00:43:21 +0530

    set comments

--- htdocs/add.php
+++ htdocs/add.php
@@ -1,30 +1,48 @@
 <html>
 
 <head>
-    <title>Add</title>
+    <title>Binary Calculator</title>
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet" />
 </head>
 
 <body>
-    <!-- Here we go... -->
-    <?php
+    <section class="vh-100">
+        <div class="container py-5">
+            <h1>Simple Binary Calculator</h1>
+            <div class="row">
+                <div class="col-xl-4 col-md-6 col-lg-4 col-sm-8 col-8 m-5">
+                    <?php
+                    parse_str(implode('&', array_slice($argv, 1)), $_GET);
+                    // print_r($_GET);                    
+                    if (isset($_POST['n1']) && isset($_POST['n2'])) {
+                        $n1 = $_POST['n1'];
+                        $n2 = $_POST['n2'];
+                        $sum = $n1 + $n2;
+                        echo `<div>Number 1 = $n1</div>
+                <div>Number 2 = $n2</div>
+                <h3 class="pt-4">Sum = $sum</h3>`;
+                    } else if (isset($_GET['n1']) && isset($_GET['n2'])) {
+                        $n1 = $_GET['n1'];
+                        $n2 = $_GET['n2'];
+                        $sum = $n1 + $n2;
+                        echo "<div>Number 1 = $n1</div>
+                <div>Number 2 = $n2</div>
+                <h3 class='pt-4'>Sum = $sum</h3>";
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    parse_str(implode('&', array_slice($argv, 1)), $_GET);
-    // print_r($_GET);
-
-    if (isset($_POST['n1']) && isset($_POST['n2'])) {
-        $n1 = $_POST['n1'];
-        $n2 = $_POST['n2'];
-        $sum = $n1 + $n2;
-        echo `Sum = $sum`;
-    } else {
-        if (isset($_GET['n1']) && isset($_GET['n2'])) {
-            $n1 = $_GET['n1'];
-            $n2 = $_GET['n2'];
-            $sum = $n1 + $n2;
-            echo "Sum = $sum";
-        }
-    }
-    ?>
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.js"></script>
 </body>
 
 </html>
--- src/HttpServer.java
+++ src/HttpServer.java
@@ -3,52 +3,62 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 public class HttpServer {
     public static void main(String[] args) throws IOException {
         int port = 2728;
         try {
+            //create a server socket
             ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("Server Listening on port " + port);
             while (true) {
+                //creare a client socket
                 Socket clientSocket = serverSocket.accept();
-                new RequestHandler(clientSocket).start();
+                //send client requst to our server
+                new requestHandler(clientSocket).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static class RequestHandler extends Thread {
+    private static class requestHandler extends Thread {
+        //intialize socket
         private Socket socket;
-        public RequestHandler(Socket socket) {
+
+        public requestHandler(Socket socket) {
+            //set client socket to socket
             this.socket = socket;
         }
+
         @Override
         public void run() {
             try {
+                //create new buffer reader for client socket requests
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 OutputStream outputStream = socket.getOutputStream();
-//                PrintWriter serverResponse = new PrintWriter(outputStream, true);
+                // PrintWriter serverResponse = new PrintWriter(outputStream, true);
                 String request = bufferedReader.readLine();
                 if (request != null) {
+                    //requst first line split with spaces
                     String[] requestParts = request.split(" ");
-                    String method = requestParts[0];
-                    String filePath = requestParts[1];
-                    String[] correctFilepath = filePath.split("\\?");
-                    filePath = "htdocs"+correctFilepath[0];
-                    if(filePath.equals("htdocs/")){
-                        filePath = "htdocs/index.php";
+                    String method = requestParts[0]; //for method
+                    String filePath = requestParts[1]; //for file path
+                    String[] correctFilepath = filePath.split("\\?"); // get only file name when parameters are available
+                    filePath = "htdocs" + correctFilepath[0]; //set htdocs path (root)
+                    if (filePath.equals("htdocs/")) {
+                        filePath = "htdocs/index.html";
                     }
-                    if (method.equals("GET") ) {
+                    if (method.equals("GET")) { //handle get requests
                         if (filePath.equals("htdocs/add.php")) {
                             String params = correctFilepath[1];
-                            executePhpScript(filePath, params,outputStream);
+                            phpHandler(filePath, params, outputStream); //for get method php forms
                         } else {
-                            serveFile(filePath, outputStream);
+                            htmlHandler(filePath, outputStream); //render html files
                         }
-                    } else if (method.equals("POST")) {
-                        handlePostRequest(bufferedReader,filePath,outputStream);
-                    } else {
+                    } else if (method.equals("POST")) { //handle post method
+                        postRequestHandler(bufferedReader, filePath, outputStream);
+                    } else { //when path doesn't exist
                         sendResponse(outputStream, "HTTP/1.1 405 Method Not Allowed", "Method Not Allowed");
                     }
                 }
@@ -61,96 +71,8 @@ public class HttpServer {
         }
     }
 
-    private static void serveFile(String filePath, OutputStream outputStream) throws IOException {
-        File file = new File(filePath);
-
-        if (file.exists() && file.isFile()) {
-            if(filePath.endsWith(".php")){
-                //Best code to render php files and get the output
-            }
-            else {
-                //fileReader object reads the input of the file in the filePath
-                BufferedReader fileReader = new BufferedReader(new FileReader(file));
-
-                //Created a stringBuilder; s a class in Java that provides a way to efficiently construct and manipulate strings
-                StringBuilder fileContent = new StringBuilder();
-                String line = "";
-
-                while ((line = fileReader.readLine()) != null) {
-                    //Get all the content in the file by appending each line the file
-                    fileContent.append(line).append("\n");
-                }
-
-                String responseHeaders = "HTTP/1.1 200 OK\r\n" +
-                        "Content-Type: text/html\r\n" +
-                        "Content-Length: " + file.length() + "\r\n" +
-                        "Connection: close\r\n\r\n";
-
-                outputStream.write(responseHeaders.getBytes());
-                outputStream.write(fileContent.toString().getBytes());
-//            sendResponse(outputStream,"HTTP/1.1 200 OK","Success");
-            }
-        } else
-        {
-            sendResponse(outputStream, "HTTP/1.1 404 Not Found", "File Not Found");
-        }
-    }
-    private static void sendResponse(OutputStream outputStream, String statusLine, String message) throws IOException {
-        String response = statusLine + "\r\n" +
-                "Content-Type: text/plain\r\n" +
-                "Content-Length: " + message.length() + "\r\n" +
-                "Connection: close\r\n\r\n" +
-                message;
-
-        outputStream.write(response.getBytes());
-    }
-
-    private static void executePhpScript(String filePath,String params,OutputStream outputStream) throws IOException {
-        try {
-            // Create a ProcessBuilder to run the PHP interpreter with the script file
-            ProcessBuilder pb = new ProcessBuilder("php", filePath ,params);
-
-            // Redirect error stream to the output stream
-            pb.redirectErrorStream(true);
-
-            // Start the PHP process
-            Process process = pb.start();
-
-            // Get the input stream of the PHP process (the output of the script)
-            InputStream scriptOutput = process.getInputStream();
-
-            // Create a buffer to read the script output
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-
-            // Write the HTTP response headers
-            String responseHeaders = "HTTP/1.1 200 OK\r\n" +
-                    "Content-Type: text/html\r\n" +
-                    "Connection: close\r\n\r\n";
-            outputStream.write(responseHeaders.getBytes());
-
-            // Read and write the script output to the client
-            while ((bytesRead = scriptOutput.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-
-            // Wait for the PHP process to complete
-            int exitCode = process.waitFor();
-
-            if (exitCode != 0) {
-                // Handle PHP execution errors if needed
-                // You can send an error response or log the error here
-            }
-
-            // Close the output stream
-            outputStream.close();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    public static void handlePostRequest(BufferedReader bufferedReader, String filePath, OutputStream outputStream) throws IOException {
-        // Read and process the POST request body
-        StringBuilder requestBody = new StringBuilder();
+    public static void postRequestHandler(BufferedReader bufferedReader, String filePath, OutputStream outputStream)
+            throws IOException {
         int contentLength = 0;
         String line;
 
@@ -172,15 +94,8 @@ public class HttpServer {
             }
             bytesRead += read;
         }
-
+        //get post request parameters
         params = new String(buffer);
-        System.out.println(params);
-
-        // Convert the request body to a string
-        String postBody = new String(buffer);
-
-        // Handle the POST data, you can parse and process it here
-        // You might want to parse form data or handle JSON/XML data, depending on your use case
 
         // Respond to the POST request
         String response = "HTTP/1.1 200 OK\r\n" +
@@ -188,7 +103,79 @@ public class HttpServer {
                 "Connection: close\r\n\r\n" +
                 "POST request processed successfully.";
 
-//        outputStream.write(response.getBytes());
-        executePhpScript(filePath, params,outputStream);
+        // outputStream.write(response.getBytes());
+        phpHandler(filePath, params, outputStream); //execute php file
+    }
+
+    private static void htmlHandler(String filePath, OutputStream outputStream) throws IOException {
+        File file = new File(filePath);
+
+        if (file.exists() && file.isFile()) {
+            // fileReader object reads the input of the file in the filePath
+            BufferedReader fileReader = new BufferedReader(new FileReader(file));
+
+            // Created a stringBuilder; s a class in Java that provides a way to efficiently construct and manipulate strings
+            StringBuilder fileContent = new StringBuilder();
+            String line ;
+            while ((line = fileReader.readLine()) != null) {
+                // Get all the content in the file by appending each line the file
+                fileContent.append(line).append("\n");
+            }
+
+            String responseHeaders = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: text/html\r\n" +
+                    "Content-Length: " + file.length() + "\r\n" +
+                    "Connection: close\r\n\r\n";
+
+            outputStream.write(responseHeaders.getBytes());
+            outputStream.write(fileContent.toString().getBytes());
+            // sendResponse(outputStream,"HTTP/1.1 200 OK","Success");
+        } else {
+            sendResponse(outputStream, "HTTP/1.1 404 Not Found", "File Not Found");
+        }
+    }
+
+    private static void phpHandler(String filePath, String params, OutputStream outputStream) throws IOException {
+        try {
+            // Create a ProcessBuilder to run the PHP interpreter with the script file
+            ProcessBuilder processBuilder = new ProcessBuilder("php", filePath, params);
+
+            // Redirect error stream to the output stream
+            processBuilder.redirectErrorStream(true);
+
+            // Start the PHP process
+            Process process = processBuilder.start();
+
+            // Get the input stream of the PHP process (the output of the script)
+            InputStream scriptOutput = process.getInputStream();
+
+            // Create a buffer to read the script output
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            // Write the HTTP response headers
+            String responseHeaders = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: text/html\r\n" +
+                    "Connection: close\r\n\r\n";
+            outputStream.write(responseHeaders.getBytes());
+
+            // Read and write the script output to the client
+            while ((bytesRead = scriptOutput.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            // Close the output stream
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendResponse(OutputStream outputStream, String statusLine, String message) throws IOException {
+        String response = statusLine + "\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "Content-Length: " + message.length() + "\r\n" +
+                "Connection: close\r\n\r\n" +
+                message;
+        outputStream.write(response.getBytes());
     }
 }
